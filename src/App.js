fea5c11e2e72d996ec0b12c360baf742fb692502
John Garrett <infantrygarrett@icloud.com>
2022-01-11 17:42:52 -0600

    converting state to redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Fragment, useEffect } from 'react';
+import React, {Fragment, useEffect} from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
-import AddBtn from './components/layout/AddBtn'
-import AddLogModal from './components/logs/AddLogModal'
-import EditLogModal from './components/logs/EditLogModal'
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from "./components/techs/AddTechModal";
-import TechListModal from "./components/techs/TechListModal"
+import TechListModal from "./components/techs/TechListModal";
+import {Provider} from 'react-redux';
+import store from './store';
 
 
 import 'materialize-css/dist/css/materialize.min.css';
@@ -17,21 +19,23 @@ const App = () => {
     useEffect(() => {
         //initialize Materialize JS
         M.AutoInit();
-    })
-  return (
-      <Fragment>
-          <SearchBar/>
-          <div className="container">
-              <AddBtn />
-              <AddLogModal />
-              <EditLogModal />
-              <AddTechModal />
-              <TechListModal/>
-              <Logs />
-          </div>
-      </Fragment>
+    });
+    return (
+        <Provider store={store}>
+            <Fragment>
+                <SearchBar/>
+                <div className="container">
+                    <AddBtn/>
+                    <AddLogModal/>
+                    <EditLogModal/>
+                    <AddTechModal/>
+                    <TechListModal/>
+                    <Logs/>
+                </div>
+            </Fragment>
+        </Provider>
 
-  );
-}
+    );
+};
 
 export default App;
