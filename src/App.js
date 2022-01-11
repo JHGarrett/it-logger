211ccb164c46b8fac7f2d37db7d1701bb074f921
John Garrett <infantrygarrett@icloud.com>
2022-01-11 14:01:03 -0600

    mutable tech list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddBtn from './components/layout/AddBtn'
 import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal"
 
 
 import 'materialize-css/dist/css/materialize.min.css';
@@ -25,6 +26,7 @@ const App = () => {
               <AddLogModal />
               <EditLogModal />
               <AddTechModal />
+              <TechListModal/>
               <Logs />
           </div>
       </Fragment>
